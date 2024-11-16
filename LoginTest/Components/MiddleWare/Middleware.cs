d3cb77c3082ea0f326e7911ef6dabd45f7d03569
Damian Czarnecki <deadsonofprejudice@outlook.com>
2024-11-16 21:48:00 +0100

    Add project files.

---
+++ LoginTest/Components/MiddleWare/Middleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Collections.Concurrent;
+using System.Security.Claims;
+
+namespace LoginTest.Components.MiddleWare
+{
+	public class Middleware
+	{
+		private readonly RequestDelegate next;
+
+		public static IDictionary<Guid, ClaimsPrincipal> Logins { get; private set; } = new ConcurrentDictionary<Guid, ClaimsPrincipal>();
+
+		public Middleware(RequestDelegate next)
+		{
+
+			this.next = next ?? throw new ArgumentException(nameof(next));
+		}
+
+		public async Task InvokeAsync(HttpContext context)
+		{
+			if (context.Request.Path == "/login" && context.Request.Query.ContainsKey("key"))
+			{
+				var key = Guid.Parse(context.Request.Query["key"]);
+				var claim = Logins[key];
+				await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claim);
+
+				context.Response.Redirect("/");
+			}
+			else
+			{
+				await next(context);
+			}
+
+
+		}
+	}
+}
---
+++ LoginTest/Program.cs
@@ -0,0 +1,49 @@
+using LoginTest.Components;
+using LoginTest.Components.MiddleWare;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/access-denied";
+    });
+builder.Services.AddAuthorization();
+
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseMiddleware<Middleware>();
+
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
